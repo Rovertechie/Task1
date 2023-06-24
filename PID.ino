178f85a23194bfbb4c2b455a47d81cd52d3361d8
Maestro <akshar.tripathi2022@vitstudent.ac.in>
2023-06-24 20:35:17 +0530

    Update PID.ino

--- PID.ino
+++ PID.ino
@@ -1,11 +1,22 @@
 const int encoderPinA = 3;       
 const int encoderPinB = 2;       
-volatile int encoderTicks = 0;   
+volatile int encoderCount = 0;   
 volatile int motorRpm = 0;
 unsigned long previousTime = 0;
 const int currentTime = 1000;
 volatile int deltaTime = 0;
 volatile int prevTime = 0;
+const int setpoint = 1000;
+volatile int proportional = 0;
+volatile int integral = 0;
+volatile int derivative = 0;
+volatile int prevError = 0;
+#define kp 2.0
+#define kd 2.0
+#define ki 2.0
+#define MIN_SPEED 0
+#define MAX_SPEED 255
+
 
 void setup() {
     pinMode(encoderPinA, INPUT);
@@ -17,7 +28,7 @@ void encoder() {
  unsigned long previousTime=millis();
  if(millis-previousTime>=currentTime){
    detachInterrupt(digitalPinToInterrupt(encoderPinA));
-   float motorRpm=(float)(encoderTicks*600.0)/996.0;
+   float motorRpm=(float)(encoderCount*600.0)/996.0;
  Serial.print("RPM is:");
  Serial.println(motorRpm);
  previousTime=millis();
@@ -31,30 +42,29 @@ void encoder() {
 void loop() {
  
   unsigned long currentTime2 = millis();
-  double deltaTime = (currentTime2 - prevTime) / 1000.0; 
-  input = motorRpm;
+  double deltaTime = (currentTime2 - prevTime) / 60000.0; 
+   int input = motorRpm;
   double error = setpoint - input;
   double proportional = kp * error;
-  double integral += ki * errorSum;
+  double integral =+ ki * error;
   double derivative = kd * (error - prevError) / deltaTime;
-  output = proportional + integral + derivative;
+  double output = proportional + integral + derivative;
   output = constrain(output, MIN_SPEED, MAX_SPEED);
   if (output >= 0) {
-    digitalWrite(DIR_PIN, HIGH); 
-    analogWrite(PWM_PIN, output); 
+    digitalWrite(encoderPinA, HIGH); 
+    analogWrite(encoderPinB, output); 
   } else {
-    digitalWrite(DIR_PIN, LOW); 
-    analogWrite(PWM_PIN, -output); 
+    digitalWrite(encoderPinA, LOW); 
+    analogWrite(encoderPinB, -output); 
   }
   prevError = error;
   prevTime = currentTime;
 
-  delay(dt * 1000); 
+  delay(1000); 
 }
 void handleEncoderInterrupt() {
   encoderCount++;
   }
-
 
 
 
