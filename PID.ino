d3a1a666183dc3cec02ad526d70ba80635de0081
Maestro <akshar.tripathi2022@vitstudent.ac.in>
2023-06-24 22:23:58 +0530

    Update PID.ino

--- PID.ino
+++ PID.ino
@@ -6,18 +6,16 @@ unsigned long previousTime = 0;
 const int currentTime = 1000;
 volatile int deltaTime = 0;
 volatile int prevTime = 0;
-const int setpoint = 1000;
+const int setpoint = 84;
 volatile int proportional = 0;
 volatile int integral = 0;
 volatile int derivative = 0;
 volatile int prevError = 0;
-#define kp 2.0
-#define kd 2.0
-#define ki 2.0
+#define kp 1.0
+#define kd 1.0
+#define ki 1.0
 #define MIN_SPEED 0
 #define MAX_SPEED 255
-
-
 void setup() {
     pinMode(encoderPinA, INPUT);
     pinMode(encoderPinB, INPUT);
@@ -28,7 +26,7 @@ void encoder() {
  unsigned long previousTime=millis();
  if(millis-previousTime>=currentTime){
    detachInterrupt(digitalPinToInterrupt(encoderPinA));
-   float motorRpm=(float)(encoderCount*600.0)/996.0;
+   float motorRpm=(float)(encoderCount*60.0)/996.0;
  Serial.print("RPM is:");
  Serial.println(motorRpm);
  previousTime=millis();
@@ -37,19 +35,20 @@ void encoder() {
  delay(100);
  }
 }
-
-
 void loop() {
+ encoder();
  
   unsigned long currentTime2 = millis();
   double deltaTime = (currentTime2 - prevTime) / 60000.0; 
    int input = motorRpm;
   double error = setpoint - input;
+  Serial.println(error);
   double proportional = kp * error;
   double integral =+ ki * error;
   double derivative = kd * (error - prevError) / deltaTime;
   double output = proportional + integral + derivative;
   output = constrain(output, MIN_SPEED, MAX_SPEED);
+  
   if (output >= 0) {
     digitalWrite(encoderPinA, HIGH); 
     analogWrite(encoderPinB, output); 
@@ -59,7 +58,6 @@ void loop() {
   }
   prevError = error;
   prevTime = currentTime;
-
   delay(1000); 
 }
 void handleEncoderInterrupt() {
